3bb59d3786d1fb6186ddc2a9cc22cc3ab7222ead
wojiaoyishang <lovepikachu@foxmail.com>
2025-01-21 16:09:27 +0800

    Update: （步骤备份）修复刷新内置标签页的问题

--- static/system/component/pear/module/tabPage.js
+++ static/system/component/pear/module/tabPage.js
@@ -383,7 +383,7 @@ layui.define(['jquery', 'element', 'dropdown'], function (exports) {
 	 */
 	tabPage.prototype.refresh = function (time) {
 
-		var $iframe = $(".layui-tab[lay-filter='" + this.option.elem + "'] .layui-tab-content .layui-show > div[data-frameid]");
+		var $iframe = $(".layui-tab[lay-filter='" + this.option.elem + "'] .layui-tab-content .layui-show > div[data-frameid], iframe[data-frameid]");
 		var $iframeLoad;
 
 		if (time != false && time != 0) {
